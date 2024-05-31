3cc64b2348845921cbc8cc981af359fe0bbfcdbe
abhinavbellamkonda <bellamkondaabhinav6@gmail.com>
2024-05-31 12:18:38 -0400

    updated deployment3 to app service

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.entity.Demo;
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static java.time.LocalDate.now;
-
 @SpringBootApplication
 public class DemoApplication  {
 
